68390a28e1b7cae19ef4ef620ba90ae5db254cee
Himanshu-co <himanshu.singh@appdynamics.com>
2019-12-04 10:41:20 +0530

    new redis routes added and more organise directories

--- helper/mysql/mysqlR.js
+++ helper/mysql/mysqlR.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql')
+// const mysql = require('mysql')
 const mysqlR = (connection, _id, callback) => {
     const queryString = 'SELECT * FROM new_table WHERE id = ?'
     connection.query(queryString, [_id], (error, rows, fields) => {
--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-const connection = require('./mysql')
+const connection = require('../helper/mysql/mysql')
 const express = require('express')
 const app = new express()
-const readOp = require('./readOp')
-const writeOp = require('./writeOp')
+const readOp = require('../helper/mongo/readOp')
+const writeOp = require('../helper/mongo/writeOp')
 const path = require('path')
-const mysqlR = require('./mysqlR')
-const mysqlW = require('./mysqlW')
-const querystring = require('query-string');
+const mysqlR = require('../helper/mysql/mysqlR')
+const mysqlW = require('../helper/mysql/mysqlW')
+const redisR = require('../helper/redis/redisR')
+const redisW = require('../helper/redis/redisW')
+const redisClient = require('../helper/redis/redisClient')
+    // const querystring = require('query-string');
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
@@ -66,6 +69,35 @@ app.post('/mysqlW', (req, res) => {
     })
 })
 
+app.get('/redisR/:key', (req, res) => {
+    const _key = req.params.key
+    redisR(redisClient, _key, (error, reply) => {
+        if (error) {
+            console.log(error)
+            return res.status(400).send(error)
+        }
+        res.send(reply)
+    })
+
+})
+
+app.post('/redisW', (req, res) => {
+    let body = ''
+    req.on('data', (chunk) => {
+        body += chunk.toString()
+    })
+
+    req.on('end', () => {
+        const parsedData = JSON.parse(body)
+        redisW(redisClient, parsedData, (error, reply) => {
+            if (error) {
+                console.log(error)
+                return res.status(400).send(error)
+            }
+            res.send(reply)
+        })
+    })
+})
 
 app.get('*', (req, res) => {
     res.send('404')
